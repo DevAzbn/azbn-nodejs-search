0860658789b7ff048d2440a5b4c5ae86b39078b9
Alexander <alexander.zybin@gmail.com>
2016-05-19 10:26:23 +0300

    201605191026

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ if(argv.help) {
 	console.log('\t--search="строка"\tИскомая строка (обяз.)'); // argv.search
 	console.log('\t--dir="строка"\t\tДиректория поиска (по умолчанию ./)'); // argv.dir
 	console.log('\t--set="строка"\t\tЗаменять на (по умолчанию не заменяет)'); // argv.set
+	console.log('\t--fmask="строка"\t\tПоиск только в файлах, в имени которых есть данная строка'); // argv.fmask
 	console.log('\t--help\t\t\tВывод этой справки'); // argv.help
 	process.exit(0);
 }
@@ -19,7 +20,8 @@ var		f_searched_txt = './searched.txt',
 		f_walk_result_json = './walk_result.json',
 		charset = 'utf8',
 		root = argv.dir ? argv.dir : './',
-		re_search = new RegExp('(' + argv.search + ')', 'ig')
+		re_search = new RegExp('(' + argv.search + ')', 'ig'),
+		fmask = argv.fmask ? new RegExp('(' + argv.fmask + ')', 'ig') : false
 ;
 
 
@@ -50,7 +52,7 @@ var searchInFile = function(file, re, set) {
 		
 		if(err) {
 			console.log(err);
-		} else if(re.test(text)) {
+		} else if(text.search(re) > -1) {
 			
 			addToSearched(path.resolve(file));
 			
@@ -91,22 +93,30 @@ var walk = function(dir, done) {
 		
 		list.forEach(function(file) {
 			
-			file = path.normalize(path.resolve(dir, file));
+			var in_masked = 0;
 			
-			fs.stat(file, function(err, stat) {
+			if(fmask != false) {
+				in_masked = file.search(fmask);//fmask.test(file);
+			}
+			
+			var _file = path.normalize(path.resolve(dir, file));
+			//console.log(':' + _file);
+			
+			fs.stat(_file, function(err, stat) {
 				
 				if (stat && stat.isDirectory()) {
 					
-					walk(file, function(err, res) {
+					walk(_file, function(err, res) {
 						results = results.concat(res);
 						if (!--pending) done(null, results);
 					});
 					
-				} else if(stat && stat.isFile()) {
+				} else if(in_masked > -1 && stat && stat.isFile()) {
 					
 					//results.push(file);
+					//console.log(':' + _file);
 					
-					searchInFile(file, re_search, argv.set);
+					searchInFile(_file, re_search, argv.set);
 					
 					if (!--pending) done(null, results);
 					
@@ -125,12 +135,14 @@ walk(root, function(err, res){
 	if(err) {
 		console.log(err);
 	} else {
-		fs.writeFile(f_walk_result_json, JSON.stringify({}), function(err) {
-			if(err) {
-				console.log(err);
+		/*
+		fs.writeFile(f_walk_result_json, JSON.stringify({}), function(_err) {
+			if(_err) {
+				console.log(_err);
 			} else {
 				console.log('Результаты поиска сохранены');
 			}
 		});
+		*/
 	}
 });
